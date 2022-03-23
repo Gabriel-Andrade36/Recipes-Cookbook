039678f2820bc386cccc0a6afce5a94417a812ff
Gabriel Andrade Correa <gabrielndcorrea@gmail.com>
2022-03-22 21:59:27 -0300

    55 - adicionado lista de receitas ao recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Bolo',
+      'Um bolo',
+      'https://t1.rg.ltmcdn.com/pt/posts/9/4/3/bolo_comum_de_liquidificador_10349_600.jpg'
+    ),
+    new Recipe(
+      'Outro Bolo',
+      'Outro bolo',
+      'https://t2.rg.ltmcdn.com/pt/posts/0/7/7/bolo_de_chocolate_simples_e_fofo_4770_orig.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,20 +1,12 @@
-import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe("Bolo","Um bolo","https://t1.rg.ltmcdn.com/pt/posts/9/4/3/bolo_comum_de_liquidificador_10349_600.jpg"),
-    new Recipe("Outro Bolo","Outro bolo","https://t2.rg.ltmcdn.com/pt/posts/0/7/7/bolo_de_chocolate_simples_e_fofo_4770_orig.jpg")
-  ]
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
